69cb945fcb5d26f56d8704e14e4222ffd01c1405
josemrecio <1330220+josemrecio@users.noreply.github.com>
2020-02-18 13:31:01 +0800

    Service creating sessions and tokens based on HelloWorld sample

--- sample/HelloWorld/index.js
+++ sample/HelloWorld/index.js
@@ -4,6 +4,21 @@
 var express = require('express');
 var OpenTok = require('../../lib/opentok');
 var app = express();
+var cors = require('cors');
+var bodyParser = require('body-parser');
+
+var whitelist = ['http://localhost:8080']
+var corsOptions = {
+  origin: function (origin, callback) {
+    if (whitelist.indexOf(origin) !== -1) {
+      callback(null, true)
+    } else {
+      callback(new Error('Not allowed by CORS'))
+    }
+  }
+}
+app.use(cors(corsOptions));
+app.use(bodyParser.json());
 
 var opentok;
 var apiKey = process.env.API_KEY;
@@ -22,28 +37,41 @@ function init() {
   });
 }
 
-// Initialize the express app
-app.use(express.static(__dirname + '/public')); //
+init();
 
 // Initialize OpenTok
 opentok = new OpenTok(apiKey, apiSecret);
 
-// Create a session and store it in the express app
-opentok.createSession(function (err, session) {
-  if (err) throw err;
-  app.set('sessionId', session.sessionId);
-  // We will wait on starting the app until this is done
-  init();
-});
-
-app.get('/', function (req, res) {
-  var sessionId = app.get('sessionId');
-  // generate a fresh token for this client
-  var token = opentok.generateToken(sessionId);
-
-  res.render('index.ejs', {
-    apiKey: apiKey,
-    sessionId: sessionId,
-    token: token
+app.post('/session', function (req, res) {
+  console.log('Generating sessionId');
+  const params = { };
+  const requestLocation = req.body.location;
+  if (requestLocation && requestLocation !== 'null') {
+    console.log('location:' + requestLocation);
+    params.location = requestLocation;
+  }
+  // Create a session
+  opentok.createSession(params, function (err, session) {
+    if (err) {
+      res.status(400).json({"error": err.toString()});
+      return;
+    }
+    sessionId = session.sessionId;
+    console.log('sessionId:' + sessionId);
+    res.json({"sessionId": sessionId});
   });
 });
+
+app.get('/session', function (req, res) {
+  const sessionId = req.query.sessionId;
+  if (!sessionId || sessionId == 'null') {
+    console.log('GET missing sessionId');
+    res.send(400);
+    return;
+  }
+  console.log('sessionId:' + sessionId);
+  // generate a fresh token for this client
+  const token = opentok.generateToken(sessionId);
+  res.json({"apiKey": apiKey, "sessionId": sessionId, "token": token});
+});
+
